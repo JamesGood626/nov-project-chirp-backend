25c7ea50414756a005110c9cb357c22e19e7a8b7
mark <markcorupe@gmail.com>
2018-11-20 10:59:00 -0700

    update back to localhost:port

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true})
     .then(() => console.log("Mongo is Running"))
     .catch(err => console.log(err));
 
