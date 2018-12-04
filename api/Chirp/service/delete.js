2c20cf95c1f9d4bfe2a21383abe5e74ca76c57db
James <jamesgood626@gmail.com>
2018-12-04 10:02:08 -0700

    Refactored deletion of chirp to use mongoose findOneAndUpdate.

--- api/Chirp/service/delete.js
+++ api/Chirp/service/delete.js
@@ -10,33 +10,47 @@ const {
   INTERNAL_SERVER_ERROR
 } = require("../../StatusCodeConstants");
 
-// Would be ideal to do it this way, need to look into the docs and Stack overflow
-// const deleteChirp = async id => {
-//   const modifiedChirp = Chirp.findOneAndUpdate(
-//     { id },
+const deleteChirp = async uuid => {
+  console.log("deleteChirp RUNNING!");
+  const modifiedChirp = await Chirp.findOneAndUpdate(
+    { uuid },
+    { deleted: true },
+    { new: true }
+  );
+  if (modifiedChirp.deleted === true) {
+    return Promise.resolve(modifiedChirp);
+  }
+  return Promise.reject("Chirp wasn't updated.");
+};
+
+// The above way of updating a chirp is more efficient than
+// retrieving the chirp, modifying the model, and then resaving it
+// to the Database as seen below.
+// return new Promise(async (resolve, reject) => {
+//   console.log("deleteChirp RUNNING!");
+//   const modifiedChirp = await Chirp.findOneAndUpdate(
+//     { uuid },
 //     { deleted: true },
-//     { new: true },
-//     modifiedChirp => {
-//       console.log("THE UPDATED CHIRP: ", modifiedChirp);
-//       return modifiedChirp;
-//     }
+//     { new: true }
 //   );
 //   console.log("modifiedChirp: ", modifiedChirp);
-// };
+//   resolve(modifiedChirp);
+// });
 
-const deleteChirp = async id => {
-  const [readErr, chirp] = await to(Chirp.findById(id));
-  if (readErr) {
-    return err;
-  }
-  chirp.deleted = true;
-  const [writeErr, updatedChirp] = await to(chirp.save());
-  return writeErr ? INTERNAL_SERVER_ERROR : NO_CONTENT;
-};
+// const deleteChirp = async id => {
+//   const [readErr, chirp] = await to(Chirp.findById(id));
+//   if (readErr) {
+//     return err;
+//   }
+//   chirp.deleted = true;
+//   const [writeErr, updatedChirp] = await to(chirp.save());
+//   return writeErr ? INTERNAL_SERVER_ERROR : NO_CONTENT;
+// };
 
 const deleteChirpIfAuthorized = async (req, res) => {
   await authorizeUser(req, res);
-  const status = await deleteChirp(req.params.id);
+  const [err, updatedChirp] = await to(deleteChirp(req.params.id));
+  const status = err ? INTERNAL_SERVER_ERROR : NO_CONTENT;
   res.send(status);
 };
 
